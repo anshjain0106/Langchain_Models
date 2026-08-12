d364762831f13f3d301ef31d896fa9096dbad92c
Ansh Jain <anshjain0106@gmail.com>
2026-08-13 02:04:20 +0530

    Exploring Runnable Branch.

---
+++ Langchain-runnables/runnable_branch.py
@@ -0,0 +1,33 @@
+from langchain_core.runnables import RunnableSequence, RunnableLambda, RunnableParallel, RunnablePassthrough, RunnableBranch
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template='Write a detailed report on {topic}.',
+    input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template='Summarize the following text: /n {text}',
+    input_variables=['text']
+)
+
+model = ChatGoogleGenerativeAI(model='gemini-3.6-flash')
+
+parser = StrOutputParser()
+
+report_gen_chain = RunnableSequence(prompt1, model, parser)
+
+branch_chain = RunnableBranch(
+    (lambda x: len(x.split()) > 500, RunnableSequence(prompt2, model, parser)),
+    RunnablePassthrough()
+)
+
+final_chain = RunnableSequence(report_gen_chain, branch_chain)
+
+final_result = final_chain.invoke({'topic': 'Cricket'})
+print(final_result)
